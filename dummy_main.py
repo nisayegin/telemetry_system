c74da0607dae5bb1a4113899ac2773f2f4655771
nisayegin <nisa.yegin@bahcesehir.edu.tr>
2025-12-24 14:22:32 +0300

    Update dummy_main.py

--- dummy_main.py
+++ dummy_main.py
@@ -1,34 +1,19 @@
 import time
-from dummy_data import (
-    dummy_speed,
-    dummy_battery_temp,
-    dummy_motor_rpm,
-    dummy_battery_voltage,
-    dummy_fault_flag,
-    dummy_telemetry_packet
-)
+from dummy_data import dummy_telemetry_packet
 from logger import log_to_csv
 
 def main():
     while True:
-        speed = dummy_speed()
-        battery_temp = dummy_battery_temp()
-        motor_rpm = dummy_motor_rpm()
-        battery_voltage = dummy_battery_voltage()
-        fault = dummy_fault_flag()
-
-        print("Speed (km/h):", speed)
-        print("Battery Temp (C):", battery_temp)
-        print("Motor RPM:", motor_rpm)
-        print("Battery Voltage (V):", battery_voltage)
-        print("Fault:", fault)
-        print("-" * 30)
-
         packet = dummy_telemetry_packet()
-        log_to_csv(packet)
-        print("Telemetry Packet:", packet)
-        print("=" * 30)
 
+        print("Telemetry Data")
+        print("-" * 30)
+        for key, value in packet.items():
+            print(f"{key}: {value}")
+
+        log_to_csv(packet)
+
+        print("=" * 30)
         time.sleep(1)
 
 if __name__ == "__main__":
